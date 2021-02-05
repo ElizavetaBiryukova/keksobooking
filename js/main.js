ed423fdba015ba1ada4e39ce1dc89e58a3bc0687
ElizavetaBiryukova <birliza@gmail.com>
2021-02-05 21:40:35 +0200

    Добавляет функцию для features и значения для address, выносит числа в переменные

--- js/main.js
+++ js/main.js
@@ -1,3 +1,93 @@
+'use strict';
+
+const OFFERS_COUNT = 10;
+
+const AVATAR = {
+  MIN: 1,
+  MAX: 8,
+};
+
+const ADDRESS = {
+  MIN: 1,
+  MAX: 600,
+};
+
+const PRICE = {
+  MIN: 1200,
+  MAX: 1000000,
+};
+
+const ROOMS = {
+  MIN: 1,
+  MAX: 5,
+};
+
+const GUESTS = {
+  MIN: 1,
+  MAX: 10,
+};
+
+const LOCATION_X = {
+  MIN: 35.65,
+  MAX: 35.7,
+};
+
+const LOCATION_Y = {
+  MIN: 139.7,
+  MAX: 139.8,
+};
+
+const NUMBER_OF_SINGS = 5;
+
+const TITLE = [
+  'Отель в Токио',
+  'Отель в Киото',
+  'Отель в Хиросиме',
+  'Отель в Осаке',
+  'Отель в Саппоро',
+];
+
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const DESCRIPTION = [
+  'Номер повышенной комфортности',
+  'Стандартный однокомнатнный номер',
+  'Однокомнатный номер категории студия c мини-кухней',
+  'Отдельные апартаменты с собственной кухней',
+];
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
 const getRandomInteger = (min, max) => {
   if (min < 0 || max < 0) {
     return -1;
@@ -18,88 +108,45 @@ const getRandomFloat = (min, max, digit) => {
   return (Math.floor(Math.random() * (max - min + min)) + min).toFixed(digit);
 };
 
-
-getRandomInteger(5, 48);
-getRandomFloat(2.1, 35.2, 8);
-
-
-// заготовки данных из задания в виде массивов
-const TITLE = [
-  'Отель в Токио',
-  'Отель в Киото',
-  'Отель в Хиросиме',
-  'Отель в Осаке',
-  'Отель в Саппоро',
-];
-const TYPE = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow',
-];
-const CHECKIN = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-const CHECKOUT = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
-const DESCRIPTION = [
-  'Номер повышенной комфортности',
-  'Стандартный однокомнатнный номер',
-  'Однокомнатный номер категории студия c мини-кухней',
-  'Отдельные апартаменты с собственной кухней',
-];
-const PHOTOS = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-]
-
-// поиск случайного элемента в переданных массивах
 const getRandomArrayElement = (elements) => {
   return elements[getRandomInteger(0, elements.length - 1)]
-}
+};
 
-// функция создающая объявление
-const createAd = () => {
+const shuffleArray = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+};
+
+const createOffers = () => {
   return {
     author: {
-      avatar: 'img/avatars/user0' + getRandomInteger(1, 8) + '.png',
+      avatar: 'img/avatars/user0' + getRandomInteger(AVATAR.MIN, AVATAR.MAX) + '.png',
     },
 
     offer: {
       title: getRandomArrayElement(TITLE),
-      // address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
-      // address: (),
-      price: getRandomInteger(1200, 1000000),
+      address: getRandomInteger(ADDRESS.MIN, ADDRESS.MAX) + ', ' + getRandomInteger(ADDRESS.MIN, ADDRESS.MAX),
+      price: getRandomInteger(PRICE.MIN, PRICE.MAX),
       type: getRandomArrayElement(TYPE),
-      rooms: getRandomInteger(1, 5),
-      guests: getRandomInteger(1, 10),
+      rooms: getRandomInteger(ROOMS.MIN, ROOMS.MAX),
+      guests: getRandomInteger(GUESTS.MIN, GUESTS.MAX),
       checkin: getRandomArrayElement(CHECKIN),
       checkout: getRandomArrayElement(CHECKOUT),
-      // features Значения не должны повторяться
-      features: getRandomArrayElement(FEATURES),
+      features: shuffleArray(FEATURES).slice(0, getRandomInteger(1, FEATURES.length)),
       description: getRandomArrayElement(DESCRIPTION),
       photos: getRandomArrayElement(PHOTOS),
     },
 
-  // location: {
-  //     const locationX = getRandomFloat(35.6, 35.7, 5);
-  //     const locationY = getRandomFloat(139.7, 139.8, 5);
-  //   }
+    location: {
+      x: getRandomFloat(LOCATION_X.MIN, LOCATION_X.MAX, NUMBER_OF_SINGS),
+      y: getRandomFloat(LOCATION_Y.MIN, LOCATION_Y.MAX, NUMBER_OF_SINGS),
+    },
   }
 }
 
-createAd();
+const similarOffers = new Array(OFFERS_COUNT).fill(null).map(() => createOffers());
+
+similarOffers();
