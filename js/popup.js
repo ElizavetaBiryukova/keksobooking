321767a9ec1da1c4864e22bcf4481ca5f89cb50d
ElizavetaBiryukova <birliza@gmail.com>
2021-02-17 19:29:30 +0200

    Добавляет вызов функции

--- js/popup.js
+++ js/popup.js
@@ -2,20 +2,15 @@ import { createOffers, TYPE_HOUSE } from './data.js';
 
 const mapCanvas = document.querySelector('#map-canvas'); //Блок для вставки похожих элементов
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup'); //Находит шаблон для копировнаия
-// const similarCard = createOffers();
-// const cardListFragment = document.createDocumentFragment();
 
 //Отрисовка шаблона с данными
 const similarCard = ({ author: { avatar }, offer: { title, address, price, type, rooms, guests, checkin, checkout, features, photos, description } }) => {
-  // const similarCards = ({ author, offer }) => {
   const cardElement = cardTemplate.cloneNode(true);
-  // const { title, address, price, type, rooms, guests, checkin, checkout } = card.offer;
-  // const { avatar } = card.author;
   cardElement.querySelector('.popup__title').textContent = title;
   cardElement.querySelector('.popup__text--address').textContent = address;
   cardElement.querySelector('.popup__text--price').textContent = `${price} ₽/ночь`;
-  cardElement.querySelector('.popup__type').textContent = TYPE_HOUSE[type];
-  cardElement.querySelector('.popup__text--capacity').textContent = `${rooms} комнаты для ${guests} + гостей`;
+  cardElement.querySelector('.popup__type').textContent = TYPE_HOUSE[type].ru;
+  cardElement.querySelector('.popup__text--capacity').textContent = `${rooms} комнаты для ${guests} гостей`;
   cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${checkin}, выезд до ${checkout}`;
   const featureList = cardElement.querySelector('.popup__features');
   featureList.innerHTML = '';
@@ -35,8 +30,7 @@ const similarCard = ({ author: { avatar }, offer: { title, address, price, type,
   }
   cardElement.querySelector('.popup__avatar').src = avatar;
   mapCanvas.appendChild(cardElement);
-
 };
 
-similarCard(createOffers[0]);
+similarCard(createOffers()[0]);
 
