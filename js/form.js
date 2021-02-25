5a2ca42eca1504b7e0279da2d3592541ba0ac566
ElizavetaBiryukova <birliza@gmail.com>
2021-02-25 20:13:43 +0200

    Добавляет исправления

--- js/form.js
+++ js/form.js
@@ -4,10 +4,10 @@ const fieldsets = document.querySelectorAll('fieldset');
 const selectsFilter = formMapElement.querySelectorAll('select');
 const addressElement = document.querySelector('#address');
 
-//Переводит страницу в неактивное состояние
-const getDisabledElements = (elements) => {
+//Переводит страницу в неактивное и активное состояния
+const setDisabledState = (elements) => {
   elements.forEach((element) => {
-    element.disabled = true;
+    element.disabled = !element.disabled;
   });
 };
 
@@ -15,25 +15,18 @@ const makesInactiveForm = () => {
   formMapElement.classList.add('map__filters--disabled');
   formAdvertElement.classList.add('ad-form--disabled');
 
-  getDisabledElements(fieldsets);
-  getDisabledElements(selectsFilter);
+  setDisabledState(fieldsets);
+  setDisabledState(selectsFilter);
 };
 
 makesInactiveForm();
-
-//Переводит страницу в активное состояние
-const getEnabledElements = (elements) => {
-  elements.forEach((element) => {
-    element.disabled = false;
-  });
-};
 
 const makesActiveForm = () => {
   formMapElement.classList.remove('map__filters--disabled');
   formAdvertElement.classList.remove('ad-form--disabled');
 
-  getEnabledElements(fieldsets);
-  getEnabledElements(selectsFilter);
+  setDisabledState(fieldsets);
+  setDisabledState(selectsFilter);
 };
 
 export { addressElement, makesActiveForm }
