306324fe7210c90b2ca2af5076c4f0db3b0cb31c
ElizavetaBiryukova <birliza@gmail.com>
2021-03-23 22:15:00 +0200

    Исправляет ошибку по критерию Д8, меняет числа на строки

--- js/error.js
+++ js/error.js
@@ -3,11 +3,11 @@ const SHOW_TIME = 6000;
 //Окно с ошибкой "не получается получить данные"
 const showModalError = (message) => {
   const alertContainer = document.createElement('div');
-  alertContainer.style.zIndex = 1000;
+  alertContainer.style.zIndex = '1000';
   alertContainer.style.position = 'absolute';
-  alertContainer.style.left = 0;
-  alertContainer.style.top = 0;
-  alertContainer.style.right = 0;
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
   alertContainer.style.padding = '25px 3px';
   alertContainer.style.fontSize = '35px';
   alertContainer.style.textAlign = 'center';
